6c02caef54aacd5ddef3bec26127680410acbae1
alfkaech-heartforge <alfkaech@gmail.com>
2025-12-08 20:29:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hello from my Python app!")
+from Flask import Flask, request; 
+app = Flask(__name__); 
+@app.route('/webhook', methods=['POST']) 
+def webhook(): 
+data = request.json; 
+print('payload:', data)'; 
+with open('log.txt','a')' as f: f.write(str(data) + '\n'); 
+return 'Logged!', 200 
